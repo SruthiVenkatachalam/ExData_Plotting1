7a382fce6b96ccf2cc83ec9fdfbb5545e8e784b0
Sruthi Venkatachalam <sruthimaf@gmal.com>
2020-07-02 17:51:17 +0530

    Commit for plot3.R

--- Plot3.R
+++ Plot3.R
@@ -7,7 +7,7 @@ data <- read.table("household_power_consumption.txt", header = T,
 # convert the date variable to Date class
 data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 
-# Subset the data
+
 data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 
 # Convert dates and times
